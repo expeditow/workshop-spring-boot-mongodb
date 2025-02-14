73b3b35b437e4ca6a0f983e47cd90fd874a694ad
expeditow <gabriel.expedito021@gmail.com>
2025-02-14 15:43:50 -0300

    Inserção de usuário com POST

--- src/main/java/com/expedito/workshopmongo/services/UserService.java
+++ src/main/java/com/expedito/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.expedito.workshopmongo.domain.User;
+import com.expedito.workshopmongo.dto.UserDTO;
 import com.expedito.workshopmongo.repository.UserRepository;
 import com.expedito.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
