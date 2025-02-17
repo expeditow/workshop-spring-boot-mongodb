f4d5d6d709cd4672c40e2ee27a3aacdb9d259ebe
expeditow <gabriel.expedito021@gmail.com>
2025-02-17 18:45:08 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/expedito/workshopmongo/services/UserService.java
+++ src/main/java/com/expedito/workshopmongo/services/UserService.java
@@ -32,6 +32,10 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 	public User fromDto(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
